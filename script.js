3fa4c3c0b46fc1f58e597c66dceef0fcaf02bb82
Mikel1991 <mikel362d@gmail.com>
2020-04-21 19:41:16 -0700

    added conditions to add-up socre and display msg. Added conditions to move to the last page (store results)

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ var choicesEl = document.getElementById("choices");
 var answer1El = document.getElementById("ans1");
 var answer2El = document.getElementById("ans2");
 var answer3El = document.getElementById("ans3");
+var displayMsg = document.getElementById("wrong-right");
 var timerEl = document.getElementById("timer");
-var scoreContainerEl = document.getElementById("ScoreContainer")
+var scoreContainerEl = document.getElementById("ScoreContainer");
 
 //Create questions Array
 
@@ -25,7 +26,7 @@ var questions = [
         a3: "<js>",
         correct: "a2",
     },{
-        q: "Choose the correct JavaScript syntaz to change the content of the following HTML code:\n &ltp id='test'&gtThis is a test </>",
+        q: "Choose the correct JavaScript syntaz to change the content of the following HTML code: <br><br> &ltp id='test'&gtThis is a test &lt/p&gt",
         a1: "document.getElementbyId('#test').innerHTML = 'I know the answer'",
         a2: "document.getElementbyId('test').innerHTML = I know the answer",
         a3: "document.getElementbyId('test').innerHTML = 'I know the answer'",
@@ -34,7 +35,7 @@ var questions = [
 ]
 
 //Create new variables
-var quizTime = 120;
+var quizTime = 100;
 var followingQuestions = 0;
 var score = 0;
 
@@ -43,7 +44,7 @@ containerEl.addEventListener("click", startQuiz);
 
 //Create the functions of questions to offer when function quiz starts
 function displayQuestions() {
-    questionsEl.textContent = questions[followingQuestions].q;
+    questionsEl.innerHTML = "<h3>" + questions[followingQuestions].q + "</h3>";
     answer1El.textContent = questions[followingQuestions].a1;
     answer2El.textContent = questions[followingQuestions].a2;
     answer3El.textContent = questions[followingQuestions].a3;
@@ -57,7 +58,9 @@ function displayTimer() {
         timerEl.textContent = "Time: "+ quizTime;
 
         if (quizTime === 0) {
-            clearInterval(timerInterval)
+            clearInterval(timerInterval);
+            //game is over and we move on to the page to save our score 
+            scoreTrackerPage();
         }
     }, 1000);
 }
@@ -72,32 +75,48 @@ function startQuiz() {
     displayTimer();
     //Change the not display status from html code and display the quiz section
     quizEl.style.display = "block";
+
+    
 }
 
 
 //Function to continue to the next question
 function nextQuestion(event) {
-    console.log(event.target);
-    console.log(event.target.value);
-
-    
-    followingQuestions++;
-    displayQuestions();
-    // if answer is right move on to next question add 1 point to the score
+    //check answers
     if (event.target.value === questions[followingQuestions].correct) {
+        //if answer is right add 1 point to the score and print "CORRECT!" on green
         score++;
-           
-    } // if answer is wrong, move on to next question and subtract 5 seconds from timer 
-    else {
-        
+        displayMsg.style.color = "green"
+        displayMsg.innerHTML = "CORRECT!"
+    } else {
+        //if answer is wrong, subtract 5 seconds from timer and print "WRONG!" on red
+        //STILL NEED TO ADD SUBTRACTION FROM TIMER
+        displayMsg.style.color = "red"
+        displayMsg.innerHTML = "WRONG!"
     }
+    //keep moving on the questions until we answer the last one. Then go to the scoreTrackerPage
+    if (followingQuestions < questions.length -1) {
+        //Move to the following question no matter if answer is right or not.
+        followingQuestions++;
+    } else {
+        scoreTrackerPage();
+    }
+    //if we keep on having questions, after adding a number to our index with followingQuestions++, que display the corresponding Q & A
+    displayQuestions();
+    
 };
-//Prueba 1
+
+//Function to show the scoreTrackerPage where we'll save our final score
+function scoreTrackerPage() {
+    containerEl.style.display = "none";
+    quizEl.style.display = "none";
+    scoreContainerEl.display = "block";
+}
+
+//Call the nextQuestion function
 answer1El.addEventListener("click", nextQuestion);
 answer2El.addEventListener("click", nextQuestion);
 answer3El.addEventListener("click", nextQuestion);
 
-console.log(followingQuestions);
-console.log(questions[followingQuestions]);
-console.log(questions[followingQuestions].correct);
+
 
