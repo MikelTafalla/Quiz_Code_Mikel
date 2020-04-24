745565b0004268afbb89edd672237b836c16412b
Mikel1991 <mikel362d@gmail.com>
2020-04-24 10:51:10 -0700

    debugged problems from previous uncommented version. Deleted commented version. Fisnish classification and local storage

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var displayScoreEl = document.getElementById("score");
 var displayFinalTimeEl = document.getElementById("finaltime");
 var recordUserEl = document.getElementById("user");
 var clasificationListEL = document.getElementById("clasification");
-// localStorage.clear();
+//localStorage.clear();
 
 //Create questions Array
 var questions = [
@@ -39,7 +39,7 @@ var questions = [
 ]
 
 //Create new variables
-var quizTime = 14;
+var quizTime = 15;
 var followingQuestions = 0;
 var score = 0;
 var timerInterval;
@@ -71,7 +71,6 @@ function displayTimer() {
     }, 1000);
 }
 
-
 //Function to start the quiz. Hide previous page. Show questions
 function startQuiz() {
     //Do not display the container section from our html
@@ -94,9 +93,9 @@ function nextQuestion(event) {
         displayMsg.innerHTML = "CORRECT!"
     } else {
         //if answer is wrong, subtract 5 seconds from timer and print "WRONG!" on red
-        //STILL NEED TO ADD SUBTRACTION FROM TIMER
         displayMsg.style.color = "red"
         displayMsg.innerHTML = "WRONG!"
+        // displayTimer().subtract(5, "seconds"); NOT WORKING
     }
     //keep moving on the questions until we answer the last one. Then go to the scoreTrackerPage
     if (followingQuestions < questions.length -1) {
@@ -121,8 +120,6 @@ function scoreTrackerPage() {
     scoreContainerEl.style.display = "block";
     displayScoreEl.innerHTML = score + " point(s)";
     displayFinalTimeEl.innerHTML = "Quiz done in " + CompletionTime + " seconds";
-    // localStorage.setItem("result", score);
-    // localStorage.setItem("finaltime", quizTime);
 }
 
 //Call the nextQuestion function
@@ -130,66 +127,10 @@ answer1El.addEventListener("click", nextQuestion);
 answer2El.addEventListener("click", nextQuestion);
 answer3El.addEventListener("click", nextQuestion);
 
-// //Below we create the classification
-// userRegister = [];
-// init();
-// //function to display the results.
-// //create function to have a clasification
-// function displayResults () {
-//     //Create list to show classification
-//     clasificationListEL.innerHTML = "";
-//     //Loop through all the users tha play
-//     for (var i= 0; i < userRegister.length; i++) {
-//         //We store each user in the variable newUserRegister
-//         var newUserRegister = userRegister[i];
-//         //Create the list elements everytime a user is logged
-//         var UserList = document.createElement("li");
-//         UserList.textContent = newUserRegister;
-//         UserList.setAttribute("data-index", i);
-//         //Append the list element to the ul in DOM
-//         clasificationListEL.appendChild(UserList)
-//     }
-// }
-// //Retrieve elements from Local storage. It's first because we want players from previous games to appear
-// function init() {
-//     //converts the string of the array in an object
-//     var storedUsers = JSON.parse(localStorage.getItem("userRegister"));
-//     //if there's no new player, the Array of players remains with the stored ones as objects
-//     if (storedUsers !== null) {
-//         userRegister = storedUsers;
-//     }
-//     //we call the function on top
-//     displayResults();
-// }
-
-// //It saves new players on the local storage
-// function saveUsers() {
-//     localStorage.setItem("userRegister", JSON.stringify(userRegister));
-// }
-
-// //Initializes the function when we press enter key
-// scoreContainerEl.addEventListener("submit", function(event) {
-//     event.preventDefault();
-//     //We create a variable that it's storing the user's input. Trim eliminates whitespace
-//     var userText = recordUserEl.value.trim();
-//     //Validation. If user doesn't input anything, stop function
-//     if (userText === "") {
-//         return;
-//     }
-//     //We push the user input inside the array of users that have played the game
-//     userRegister.push(userText);
-//     recordUserEl.value = "";
-//     //After we press enter we hide the input name field and we show the clasification
-//     recordUserEl.style.display = "none";
-//     displayScoreEl.style.display = "none";
-//     displayFinalTimeEl.style.display = "none";
-//     saveUsers();
-//     displayResults();
-// })
-
-
 //Empty Array that we'll fill with user's input
 var Users = [];
+var Results = [];
+var Time = [];
 
 //Create function to store user's input (name)
 function storeUsers(event) {
@@ -198,8 +139,12 @@ function storeUsers(event) {
     var userSaved = recordUserEl.value;
     // Create Array of Users
     Users.push(userSaved);
+    Results.push(score);
+    Time.push(CompletionTime);
     //Make a string from the Users in the Array
     localStorage.setItem("username", JSON.stringify(Users));
+    localStorage.setItem("results", JSON.stringify(Results));
+    localStorage.setItem("finaltime", JSON.stringify(Time));
     //So the function below show previously stored items
     ClasificationTable()
      //After we press enter we hide the input name field and we show the clasification
@@ -211,15 +156,26 @@ function storeUsers(event) {
 //Create the list of people that played
 function ClasificationTable() {
     //Convert the String into a JSON object
-    var Users = JSON.parse(localStorage.getItem("username"));
+    Users = JSON.parse(localStorage.getItem("username"));
+    Results = JSON.parse(localStorage.getItem("results"));
+    Time = JSON.parse(localStorage.getItem("finaltime"));
+    
+    //If username doesn't exist in localstorage Users will be null. If Users is null, Users.length will throw an error. If Users = null, initialize Users anyway.
+    if (Users == null) {
+        Users = [];
+        Results = [];
+        Time = [];
+     }
     // Loop through the array of usernames
     for (var i=0; i < Users.length; i++) {
         //Store each user in the variable
         var registeredUser = Users[i];
+        var registeredResults = Results[i];
+        var registeredTime = Time[i];
         // I want to call them individually and then display in the list
         // print the information in a list, print every name we have in local storage
         var List = document.createElement("li");
-        List.textContent = registeredUser + ": " + score + " points in " + CompletionTime + " seconds";
+        List.textContent = registeredUser + ": " + registeredResults + " points in " + registeredTime + " seconds";
         clasificationListEL.appendChild(List);
     }
 }
@@ -227,3 +183,6 @@ function ClasificationTable() {
 //When you click enter, the event is "submit" and we want that eventListener to be added to scoreContainerEl.
 scoreContainerEl.addEventListener("submit", storeUsers);
 ClasificationTable();
+
+
+
